00efac5a73ca6ff999de8c12c5a752c368e90414
Burak UĞRANLI <burak.ugranli@yemeksepeti.com>
2022-02-03 15:14:35 +0300

    initial commit

---
+++ channel-assessment-repo/ChannelEngineLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ChannelEngineLibrary
+{
+    public class Class1
+    {
+    }
+}
---
+++ channel-assessment-repo/channel-assessment-repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace channel_assessment_repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
