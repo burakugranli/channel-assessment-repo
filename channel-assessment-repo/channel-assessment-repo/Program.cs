96f26e9683a86e6dabe83e5cd4ce5f5a9f0f16a5
Burak UĞRANLI <burak.ugranli@yemeksepeti.com>
2022-02-03 17:17:38 +0300

    console app dependency injection

--- channel-assessment-repo/channel-assessment-repo/Program.cs
+++ channel-assessment-repo/channel-assessment-repo/Program.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using ChannelEngineLibrary.ApiClient;
+using ChannelEngineLibrary.Configuration;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
 
 namespace channel_assessment_repo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async System.Threading.Tasks.Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            IServiceCollection serviceCollection = Initialize();
+            ServiceProvider serviceProvider = serviceCollection.BuildServiceProvider();
+
+            IApiClient client = serviceProvider.GetService<IApiClient>();
+
+        }
+
+        private static IServiceCollection Initialize()
+        {
+            var builder = new ConfigurationBuilder()
+                  .SetBasePath(Directory.GetCurrentDirectory())
+                  .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                  .AddEnvironmentVariables();
+            IConfigurationRoot configuration = builder.Build();
+
+            IServiceCollection serviceCollection = new ServiceCollection();
+            serviceCollection.AddSingleton<IConfiguration>(configuration);
+            serviceCollection.AddSingleton<IApiClientConfiguration, ApiClientConfiguration>();
+            serviceCollection.AddSingleton<IApiClient, ApiClient>();
+
+            return serviceCollection;
         }
     }
 }
