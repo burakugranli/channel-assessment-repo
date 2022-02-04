466652af8f27e0a58feb057d0f19a02c6123aea0
Burak UĞRANLI <burak.ugranli@yemeksepeti.com>
2022-02-04 16:20:25 +0300

    webapp

---
+++ channel-assessment-repo/ChannelEngineWebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using ChannelEngineLibrary.Model;
+using ChannelEngineLibrary.Service;
+using ChannelEngineWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChannelEngineWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly IProductService productService;
+
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
+        {
+            _logger = logger;
+            this.productService = productService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<ProductResponse> productResponses = await this.productService.GetTop5ProductFromOrders();
+            return View(productResponses.ToList());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ channel-assessment-repo/ChannelEngineWebApp/Startup.cs
@@ -0,0 +1,75 @@
+using ChannelEngineLibrary.ApiClient;
+using ChannelEngineLibrary.Configuration;
+using ChannelEngineLibrary.Service;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChannelEngineWebApp
+{
+    public class Startup
+    {
+        public Startup(IWebHostEnvironment env)
+        {
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder()
+              .SetBasePath(env.ContentRootPath)
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+              .AddEnvironmentVariables();
+
+            this.Configuration = configurationBuilder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+
+            services.AddSingleton<IApiClientConfiguration>((serviceProvider) =>
+            {
+                return new ApiClientConfiguration((IConfigurationRoot)this.Configuration);
+            });
+
+            services.AddSingleton<IProductService, ProductService>();
+            services.AddSingleton<IApiClientConfiguration, ApiClientConfiguration>();
+            services.AddSingleton<IApiClient, ApiClient>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
